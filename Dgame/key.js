dd7c1e98e271fdf25ecc44de08623778d4357714
wendy8615 <wendy8615@yahoo.com.tw>
2017-07-25 22:44:17 +0800

    yawen1991

---
+++ Dgame/key.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+function move(){
+	ek=event.keyCode;
+	if (ek==37) car.style.posLeft-=5;
+	if (ek==39) car.style.posLeft+=5;
+	if (ek==38) car.style.posTop-=5;
+	if (ek==40) car.style.posTop+=5;
+}
+
+
